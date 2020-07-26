54c27c6a8cf2bd3069bf1dd91718b551e067edcc
Alexandr <sasha393@bk.ru>
2020-07-26 13:48:17 +0300

    css styles was added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './Header';
+import { HomePage } from './HomePage';
+import { gray2, fontSize, fontFamily } from './Styles';
 
-function App() {
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+/*function App() {
   debugger;
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
-}
+}*/
+const App: React.FC = () => {
+  return (
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
+      <Header />
+      <HomePage />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Icons.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import user from './user.svg';
+
+//export const UserIcons = () =>
+//<img src=(user) alt="User" width="12px" />;
