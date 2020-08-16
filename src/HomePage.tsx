d89ade241d1f8b0556a51cd2d86932ad74dbfa38
Alexandr <sasha393@bk.ru>
2020-08-16 22:23:16 +0300

    Hompage and header components had been installed

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -3,36 +3,67 @@ import { PrimaryButton } from './Styles';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { QuestionList } from './QuestionList';
-import { getUnansweredQuestions } from './QuestionData';
+import { getUnansweredQuestions, QuestionData } from './QuestionData';
+import { useEffect, useState } from 'react';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
+import { async } from 'q';
 
-export const HomePage = () => (
-  <div
-    css={css`
-      margin: 50px auto 20px auto;
-      padding: 30px 20px;
-      max-width: 600px;
-    `}
-  >
-    <div
-      css={css`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-      `}
-    >
-      <h2
+const renderQuestion = (question: QuestionData) => <div>{question.title}</div>;
+
+export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[] | null>(null);
+  const [questionsLoading, setQuestionsLoading] = useState(true);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    //  console.log('first rendered');
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  });
+  const handleAskQuestionClick = () => {
+    setCount(count + 1);
+    console.log('TODO - move to the AskPage');
+  };
+
+  return (
+    <Page>
+      <div
         css={css`
-          font-size: 15px;
-          font-weight: bold;
-          margin: 10px 0px 5px;
-          text-align: center;
-          text-transform: uppercase;
+          margin: 50px auto 20px auto;
+          padding: 30px 20px;
+          max-width: 600px;
         `}
       >
-        Unanswered Questions
-      </h2>
-      <PrimaryButton>Ask a question</PrimaryButton>
-    </div>
-    <QuestionList data={getUnansweredQuestions()} />
-  </div>
-);
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          `}
+        >
+          <PageTitle>Unanswered Questions</PageTitle>
+
+          <PrimaryButton onClick={handleAskQuestionClick}>
+            Ask a question
+          </PrimaryButton>
+        </div>
+        {questionsLoading ? (
+          <div
+            css={css`
+              font-size: 16px;
+              font-style: italic;
+            `}
+          >
+            Loading...
+          </div>
+        ) : (
+          <QuestionList data={questions || []} />
+        )}
+      </div>
+    </Page>
+  );
+};
--- src/QuestionList.tsx
+++ src/QuestionList.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { gray5, accent2 } from './Styles';
@@ -8,33 +8,37 @@ import React from 'react';
 
 interface Props {
   data: QuestionData[];
+  renderItem?: (item: QuestionData) => JSX.Element;
 }
 
-export const QuestionList: FC<Props> = (props) => (
-  <ul
-    css={css`
-      list-style: none;
-      margin: 10px 0 0 0;
-      padding: 0px 20px;
-      background-color: #fff;
-      border-bottom-left-radius: 4px;
-      border-bottom-right-radius: 4px;
-      border-top: 3px solid ${accent2};
-      box-shadow: 0 3px 5px rgba(0, 0, 0, 0.16);
-    `}
-  >
-    {props.data.map((question) => (
-      <li
-        key={question.questionId}
-        css={css`
-          border-top: 1px solid ${gray5};
-          :first-of-type {
-            border-top: none;
-          }
-        `}
-      >
-        <Question data={question} />
-      </li>
-    ))}
-  </ul>
-);
+export const QuestionList: FC<Props> = memo(({ data, renderItem }) => {
+  console.log('Rendering QuestionList', data, renderItem);
+  return (
+    <ul
+      css={css`
+        list-style: none;
+        margin: 10px 0 0 0;
+        padding: 0px 20px;
+        background-color: #fff;
+        border-bottom-left-radius: 4px;
+        border-bottom-right-radius: 4px;
+        border-top: 3px solid ${accent2};
+        box-shadow: 0 3px 5px rgba(0, 0, 0, 0.16);
+      `}
+    >
+      {data.map((question) => (
+        <li
+          key={question.questionId}
+          css={css`
+            border-top: 1px solid ${gray5};
+            :first-of-type {
+              border-top: none;
+            }
+          `}
+        >
+          {renderItem ? renderItem(question) : <Question data={question} />}
+        </li>
+      ))}
+    </ul>
+  );
+});
