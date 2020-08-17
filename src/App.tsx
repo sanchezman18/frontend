962548061a2f632fbf9e8302f6d2a6d774e99187
Alexandr <sasha393@bk.ru>
2020-08-17 21:56:38 +0300

    Not found page has been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+//import { NotFoundPage } from './NotFoundPage';
 import { gray2, fontSize, fontFamily } from './Styles';
 
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-//  180
+import { NotFoundPage } from './NotFoundPage';
+//  196
 /*function App() {
   debugger;
   return (
@@ -18,16 +24,24 @@ import { css, jsx } from '@emotion/core';
 }*/
 const App: React.FC = () => {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/ask" component={AskPage} />
+          <Route path="/signin" component={SignInPage} />
+          <Route component={NotFoundPage} />
+        </switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
