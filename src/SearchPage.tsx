6d456cea9513fba4c93400e3c5a666e04cb4c64a
Alexandr <sasha393@bk.ru>
2020-09-02 22:54:30 +0300

    SearchPage has been changed

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,4 +1,17 @@
 import React from 'react';
+import { FC, useState, useEffect } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import { Page } from './Page';
+import { QuestionList } from './QuestionList';
+import { searchQuestions, QuestionData } from './QuestionData';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 
-export const SearchPage = () => <Page title="Search Result" />;
+export const SearchPage: FC<RouteComponentProps> = ({ location }) => {
+  const [questions, setQuestions] = useState<QuestionData[]>([]);
+
+  const searchParams = new URLSearchParams(location.search);
+  const search = searchParams.get('criteria') || '';
+
+  return <Page title="Search Results" />;
+};
